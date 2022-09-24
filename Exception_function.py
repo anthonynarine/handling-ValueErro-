be1c273e5516879e92d36b9e99021279cf30eec9
Anthony Narine <anarine83@gmail.com>
2022-09-24 02:49:28 -0400

    Small function refinements made

--- Exception_function.py
+++ Exception_function.py
@@ -1,19 +1,17 @@
 
 # let make a function from our get in program (see esceptions.py)
 
-
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            x = int(input("What's x? "))
+            x = int(input(prompt))
         except ValueError:
             print("x should be an integer")
         else:
-            break
-    return x
+            return x  #return can be used to both break and return a value
 
 main()
